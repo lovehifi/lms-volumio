627558189b4199510e3656a51afbc4b4820b57cf
lovehifi <81028393+lovehifi@users.noreply.github.com>
2024-03-01 03:48:07 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # cd /tmp && wget https://raw.githubusercontent.com/lovehifi/lms-volumio/main/install.sh && sudo chmod -R 755 /tmp/install.sh && ./install.sh
 cd ~
+echo "Download the LMS plugin from Volumio repo"
 git clone https://github.com/volumio/volumio-plugins-sources.git
-echo "Install LMS 8.5 to Volumio"
 
 file_path="/home/volumio/volumio-plugins-sources/lms/install.sh"
-
 awk '/for f in \/home\/volumio\/logitechmediaserver\/logitechmediaserver\*\.deb; do dpkg -i "\$f"; done/ { 
     print "cd /tmp";
     print "apt-get install libio-socket-ssl-perl lame unzip -y";
@@ -16,10 +15,11 @@ awk '/for f in \/home\/volumio\/logitechmediaserver\/logitechmediaserver\*\.deb;
 
 #file_path="/home/volumio/volumio-plugins-sources/lms/install.sh" && awk '/for f in \/home\/volumio\/logitechmediaserver\/logitechmediaserver\*\.deb; do dpkg -i "\$f"; done/ { print "cd /tmp"; print "apt-get install libio-socket-ssl-perl lame unzip -y"; print "wget https://downloads.slimdevices.com/nightly/logitechmediaserver_8.5.0~1709149164_all.deb"; print "dpkg -i /tmp/logitechmediaserver_8.5.0~1709149164_all.deb"; next } 1' "$file_path" > temp_file && mv temp_file "$file_path"
 
-
+echo "Install LMS 8.5 to Volumio"
 cd /home/volumio/volumio-plugins-sources/lms
 volumio plugin install
 mkdir -p /var/lib/squeezeboxserver/prefs/material-skin
 
+echo "SQ MC to Volumio"
 cd /home/volumio/volumio-plugins-sources/squeezelite_mc
 volumio plugin install
